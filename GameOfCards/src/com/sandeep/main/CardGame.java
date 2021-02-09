6aa896a05ddfff8a2d7d639ae43d451d2baf5db3
sandeep_ray <sandeep.ray786@gmail.com>
2021-02-10 01:19:29 +0530

    initial setup commit

---
+++ GameOfCards/src/com/sandeep/main/CardGame.java
@@ -0,0 +1,10 @@
+package com.sandeep.main;
+
+public class CardGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Game of Cards");
+	}
+
+}
